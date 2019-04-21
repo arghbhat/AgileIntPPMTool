68033bb35cd0c8d96e023c1702b9ecc18c777527
Arghya <arghya045@gmail.com>
2019-04-22 01:21:58 +0530

    Proj first commit

---
+++ Spring_Backend/PPMToolBackEnd/src/main/java/com/arghya/tool/springbackend/PpmToolBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.arghya.tool.springbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolBackEndApplication.class, args);
+	}
+
+}
